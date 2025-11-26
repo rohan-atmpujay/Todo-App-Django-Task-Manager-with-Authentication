ab98e3806ddcf04f448d3313ed18c0e605ed09a5
Rohan Atmpujay <rohanatmpujay@example.com>
2025-11-26 18:19:23 +0530

    Added requirments.txt

--- todo_list/todo_list/settings.py
+++ todo_list/todo_list/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/5.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -25,7 +26,7 @@ SECRET_KEY = "django-insecure-*2*6a2%b!*ef0*4v$pe$ldmxy-!qlbcc*q-ai)bfq#gj*(3ren
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -134,3 +135,6 @@ AUTHENTICATION_BACKENDS = [
 
 # Added manually
 LOGIN_URL = '/login/'
+
+# Added manually
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
